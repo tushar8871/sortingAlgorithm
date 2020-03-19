f1c65c2bda8433bae21b32d1a9eb324a4f72baa5
tushar8871 <tusharkhachane6@gmail.com>
2020-03-19 10:54:54 +0530

    Added : use case 5 : compute expression 4 i.e a%b+c

--- sortingAlgorithm.sh
+++ sortingAlgorithm.sh
@@ -4,11 +4,14 @@ read -p "Enter a :" num1
 read -p "Enter b :" num2
 read -p "Enter c :" num3
 
+echo "expression 1 :"
 result=$(( $num1 + $num2 * $num3));
-echo "expression 1 : $result"
 
+echo "expression 2 :"
 result=$(($num1*$num2+$num3));
-echo "expression 2 : $result"
 
+echo "expression 3 : "
 result=$(($num3+$num1/$num2));
-echo "expression 3 : $result"
+
+echo "expression 4 :"
+result=$(($num1%$num2+$num3));
